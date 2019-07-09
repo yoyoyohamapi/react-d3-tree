ec5e1f9840200663bcde52eca7a7bbe4675ade40
消珥 <xiaoer.wxj@alibaba-inc.com>
2019-07-09 20:53:59 +0800

    types

--- react-d3-tree.d.ts
+++ react-d3-tree.d.ts
@@ -1,64 +1,69 @@
-declare module "react-d3-tree" {
+declare module "react-d3-tree-advanced" {
   type NodeSvgShape = {
-    shape?: string,
-    shapeProps?: object
-  }
+    shape?: string;
+    shapeProps?: object;
+  };
 
   type ReactD3TreeItem = {
-    name?: string,
+    name?: string;
     attributes?: {
-      [key: string]: string,
+      [key: string]: string;
     };
-    children?: ReactD3TreeItem[],
-    _collapsed?: boolean,
-    nodeSvgShape?: NodeSvgShape
+    children?: ReactD3TreeItem[];
+    _collapsed?: boolean;
+    nodeSvgShape?: NodeSvgShape;
   };
 
   type ReactD3TreeProps = {
-    data: ReactD3TreeItem[] | ReactD3TreeItem,
-    nodeSvgShape?: NodeSvgShape,
-    nodeLabelComponent?: object,
-    onClick?: (targetNode: ReactD3TreeItem, event: Event) => any,
-    onMouseOver?: (targetNode: ReactD3TreeItem, event: Event) => any,
-    onMouseOut?: (targetNode: ReactD3TreeItem, event: Event) => any,
-    onUpdate?: (targetNode: ReactD3TreeItem, event: Event) => any,
-    orientation?: "horizontal" | "vertical",
+    data: ReactD3TreeItem[] | ReactD3TreeItem;
+    nodeSvgShape?: NodeSvgShape;
+    nodeLabelComponent?: object;
+    onClick?: (targetNode: ReactD3TreeItem, event: Event) => any;
+    onMouseOver?: (targetNode: ReactD3TreeItem, event: Event) => any;
+    onMouseOut?: (targetNode: ReactD3TreeItem, event: Event) => any;
+    onUpdate?: (targetNode: ReactD3TreeItem, event: Event) => any;
+    orientation?: "horizontal" | "vertical";
     translate?: {
-      x?: number,
-      y?: number
-    },
-    pathFunc?: ("diagonal" | "elbow" | "straight") | ((...args: any[]) => any),
-    transitionDuration?: number,
-    depthFactor?: number,
-    collapsible?: boolean,
-    useCollapseData?: boolean,
-    initialDepth?: number,
-    zoomable?: boolean,
-    zoom?: number,
+      x?: number;
+      y?: number;
+    };
+    pathFunc?: ("diagonal" | "elbow" | "straight") | ((...args: any[]) => any);
+    transitionDuration?: number;
+    depthFactor?: number;
+    collapsible?: boolean;
+    useCollapseData?: boolean;
+    initialDepth?: number;
+    zoomable?: boolean;
+    zoom?: number;
     scaleExtent?: {
-      min?: number,
-      max?: number
-    },
+      min?: number;
+      max?: number;
+    };
     nodeSize?: {
-      x?: number,
-      y?: number
-    },
+      x?: number;
+      y?: number;
+    };
     separation?: {
-      siblings?: number,
-      nonSiblings?: number
-    },
-    textLayout?: object,
-    allowForeignObjects?: boolean,
-    shouldCollapseNeighborNodes?: boolean,
-    circleRadius?: number,
+      siblings?: number;
+      nonSiblings?: number;
+    };
+    textLayout?: object;
+    allowForeignObjects?: boolean;
+    shouldCollapseNeighborNodes?: boolean;
+    circleRadius?: number;
     styles?: {
-      nodes?: object,
-      links?: object
-    }
+      nodes?: object;
+      links?: object;
+    };
+    getLinkStyle?: (linkData: {
+      source: ReactD3TreeItem;
+      target: ReactD3TreeItem;
+    }) => CSSStyleDeclaration;
+    resetOnDataChanged?: boolean;
   };
 
   var Tree: React.ComponentClass<ReactD3TreeProps>;
-  
-  export {Tree};
+
+  export { Tree };
   export default Tree;
 }
